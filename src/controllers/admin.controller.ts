97e748308e82b0631d6708b240a55409ca7e5b8e
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-03-11 12:19:18 +0530

    Change route end point name and function name

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -2,10 +2,10 @@ import { RequestHandler } from "express";
 import userModel, { ERoles } from "../models/user.model";
 import bcrypt from "bcryptjs";
 
-export const getMe: RequestHandler = async (req, res, next) => {
+export const getMyProfie: RequestHandler = async (req, res, next) => {
   try {
     const userId = res.locals.userId;
-    const user = await userModel.findById(userId);
+    const user = await userModel.findById(userId).select("-password");
     if (!user) return res.status(404).json({ success: false, message: "User not found." });
     res.status(200).json({ success: true, message: "User fetched.", user });
   } catch (error) {
--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { addAgent, getAllAgents, getMe, updateAgent } from "../controllers/admin.controller";
+import { addAgent, getAllAgents, getMyProfie, updateAgent } from "../controllers/admin.controller";
 import { authorize } from "../middlewares/authorize";
 import { validateInput } from "../middlewares/validateInput";
 import { registerUserSchema, updateAgentSchema } from "../validationSchema";
 
 const adminRouter = Router();
 
-adminRouter.get("/get-me", getMe);
+adminRouter.get("/", getMyProfie);
 adminRouter.get("/get-agents", getAllAgents);
 adminRouter.post("/add-agent", authorize(["ADMIN"]), validateInput(registerUserSchema), addAgent);
 adminRouter.post("/update-agent/:id", authorize(["ADMIN"]), validateInput(updateAgentSchema), updateAgent);
