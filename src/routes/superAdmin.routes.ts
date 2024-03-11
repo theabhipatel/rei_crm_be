fde423d2038dde60e829a1e1fd46401a340b0808
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-03-11 14:16:37 +0530

    Validate input schema for admin routes

--- src/routes/superAdmin.routes.ts
+++ src/routes/superAdmin.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { addAdmin, getAllAdmins, getMyProfile, updateAdmin } from "../controllers/superAdmin.controller";
+import { validateInput } from "../middlewares/validateInput";
+import { addAdminSchema, updateAdminSchema } from "../validationSchema";
 
 const superAdminRouter = Router();
 
 superAdminRouter.get("/", getMyProfile);
 superAdminRouter.get("/get-admins", getAllAdmins);
-superAdminRouter.post("/add-admin", addAdmin);
-superAdminRouter.patch("/update-admin/:id", updateAdmin);
+superAdminRouter.post("/add-admin", validateInput(addAdminSchema), addAdmin);
+superAdminRouter.patch("/update-admin/:id", validateInput(updateAdminSchema), updateAdmin);
 
 export default superAdminRouter;
--- src/validationSchema/index.ts
+++ src/validationSchema/index.ts
@@ -24,3 +24,23 @@ export const updateAgentSchema = object({
     isBlocked: boolean().optional(),
   }),
 });
+
+// ###########  Super admin's routes input validation Schema #######
+
+export const addAdminSchema = object({
+  body: object({
+    fullname: string({ required_error: "fullname is required." }),
+    email: string({ required_error: "email is required." }).email(),
+    password: string({ required_error: "password is required." }).min(4, "password must be 4 char long."),
+  }),
+});
+
+export const updateAdminSchema = object({
+  body: object({
+    fullname: string().optional(),
+    email: string().email().optional(),
+    password: string().min(4, "password must be 4 char long.").optional(),
+    isVerified: boolean().optional(),
+    isBlocked: boolean().optional(),
+  }),
+});
