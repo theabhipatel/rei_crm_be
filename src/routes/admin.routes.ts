7570cd5a2ea1ca87b36fa563f409f7c7b84e4ed9
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-03-11 12:01:10 +0530

    Removed authorize middleware where it was put unneccessary

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -6,8 +6,8 @@ import { registerUserSchema, updateAgentSchema } from "../validationSchema";
 
 const adminRouter = Router();
 
-adminRouter.get("/get-me", authorize(["SUPER_ADMIN", "ADMIN", "AGENT"]), getMe);
-adminRouter.get("/get-agents", authorize(["SUPER_ADMIN", "ADMIN", "AGENT"]), getAllAgents);
+adminRouter.get("/get-me", getMe);
+adminRouter.get("/get-agents", getAllAgents);
 adminRouter.post("/add-agent", authorize(["ADMIN"]), validateInput(registerUserSchema), addAgent);
 adminRouter.post("/update-agent/:id", authorize(["ADMIN"]), validateInput(updateAgentSchema), updateAgent);
 
