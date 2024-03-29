2a443a0dc20844daffefcca49df39919eeaaefba
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-03-29 11:33:07 +0530

    Update addAdminSchema for input validation and added firstName and lastName instead of full name

--- src/validationSchema/index.ts
+++ src/validationSchema/index.ts
@@ -30,7 +30,8 @@ export const updateAgentSchema = object({
 
 export const addAdminSchema = object({
   body: object({
-    fullname: string({ required_error: "fullname is required." }),
+    firstName: string({ required_error: "firstName is required." }),
+    lastName: string({ required_error: "lastName is required." }),
     email: string({ required_error: "email is required." }).email(),
     password: string({ required_error: "password is required." }).min(4, "password must be 4 char long."),
   }),
